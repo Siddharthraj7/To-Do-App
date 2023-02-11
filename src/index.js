6e7d16dc5a4d00d98b895fcd072e4c7aef2f59cf
Siddharth Raj <96586825+Siddharthraj7@users.noreply.github.com>
2023-02-11 21:47:06 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,94 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import './index.css';
+
+class AddTask extends React.Component{
+    handleAddTaskClick(){
+
+    }
+    handleAddTaskClick(){
+
+    }
+    render()
+    {
+        return(
+            <form>
+                <input type = "text" onChange={()=>this.handleAddTaskClick()}/>
+                <input type = "button" value="Add Task" onClick={()=>this.handleAddTaskClick()}/>
+            </form>
+        );
+    }
+}
+
+class TaskList extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render()
+    {
+        let list=[];
+        for(let i=0; i<this.props.tasks.length; i++){
+            let task = this.props.tasks[i];
+            let spanAction;
+            if(task.isFinished){
+                spanAction = (
+                    <span class="material-icons">close</span>
+                );
+            }else{
+                spanAction=(
+                    <span class= "material-icons">undo</span>
+                );
+            } 
+            let listItem = (
+            <div key={i}>
+                <span>{task.desc}</span>
+                {spanAction}
+                </div>
+                );
+            list.push(listItem);
+        }
+        return(
+            <div className={this.props.forstyling}>
+                <div className="list-container">
+                <div className = 'title'>{this.props.purpose}</div>
+                <div className='content'>
+                    {list}
+                </div>
+                </div>
+                </div>
+        );
+    }
+}
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+            this.state= {
+                tasks:[{
+                    desc:'Switch off light',
+                    isFinished: false
+                },{desc:'Turn on fan',
+                isFinished: false}, {desc:'Make tea',
+                isFinished: true}, {desc:'Make dinner',
+                isFinished: true}]
+            }
+    } 
+    render(){
+        let tasks = this.state.tasks;
+        let todoTasks = tasks.filter(t=>t.isFinished==false);
+        let doneTasks = tasks.filter(t=>t.isFinished==true);
+        return(
+            <>
+            <div className="add-task">
+                <AddTask />
+            </div>
+            <div className="task-lists">
+                <TaskList tasks={todoTasks} purpose="Todo" forstyling="todo" />
+                <TaskList tasks={doneTasks} purpose="Finished" forstyling="finished"/>
+            </div>
+                
+            </>
+        );
+    }
+}
+ReactDOM.render(<App />, document.getElementById("root"));
